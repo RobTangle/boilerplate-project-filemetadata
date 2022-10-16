0618fadcb5b3fe29f9c9d7a93bfc068fc8e728dd
Robert0101 <rob.tangled@gmail.com>
2022-10-15 21:34:21 -0300

    feat/Completar funcionalidad

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 var app = express();
 
@@ -14,6 +16,19 @@ app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  try {
+    // console.log(req.file);
+    return res.status(200).send({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    });
+  } catch (error) {
+    return res.status(400).send({ error: error.message });
+  }
 });
 
 const port = process.env.PORT || 3000;
